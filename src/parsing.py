9e3bde58a602d1f31df5f0dfed8b9b89951cd538
mvnathan <mvnathan@gmail.com>
2026-08-08 13:14:59 -0500

    Import timezone from datetime module

--- src/parsing.py
+++ src/parsing.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import zoneinfo
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Any
 
 UTC = zoneinfo.ZoneInfo("UTC")
